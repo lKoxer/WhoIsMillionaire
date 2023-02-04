e54f2e80954a790efc02321aeb2b00758017cb76
Alex <102607576+lKoxer@users.noreply.github.com>
2023-02-04 23:37:31 +0100

    Подготовка под UiKit

---
+++ WhoIsMillionaire/View/StartViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartViewController.swift
+//  WhoIsMillionaire
+//
+//  Created by Kox on 04.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    
+}
