3bd86d74adc61c246fbb7186010d1220874f9212
nikolay1414 <123818046+nikolay1414@users.noreply.github.com>
2023-02-05 15:25:45 +0300

    1 download

---
+++ WhoIsMillionaire/WhoIsMillionaire/View/StartViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartViewController.swift
+//  WhoIsMillionaire
+//
+//  Created by Kox on 04.02.2023.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
---
+++ WhoIsMillionaire/WhoIsMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoIsMillionaire
+//
+//  Created by Kox on 04.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
