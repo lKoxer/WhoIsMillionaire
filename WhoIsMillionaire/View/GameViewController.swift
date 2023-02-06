02a4b58eafa36c22b90cf625001644bea2a54341
Pablo <pawellreshet@icloud.com>
2023-02-06 11:57:55 +0300

    Redesign of views, timer works, sound plays

--- WhoIsMillionaire/View/GameViewController.swift
+++ WhoIsMillionaire/View/GameViewController.swift
@@ -15,16 +15,57 @@ class GameViewController: UIViewController {
     var fiftyFiftyButton = UIButton(type: .system)
     var hallHelpButton = UIButton(type: .system)
     var callToFriendButton = UIButton(type: .system)
+    
+    // таймер
+    var timer = Timer()
+    var totalTime = 30
+    var secondPassed = 0
+    
+    // аудио
     var player = AVAudioPlayer() // player for sounds
+    
+    // лейбл таймера
+    var timerLabel: UILabel = {
+        let label = UILabel()
+        label.layer.borderColor = .init(red: 1, green: 0.5, blue: 0.5, alpha: 1)
+        label.layer.borderWidth = 4
+        label.text = "30"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.frame = CGRect(x: 0, y: 0, width: 71, height: 71)
+        label.layer.cornerRadius = label.frame.width / 2
+        label.font = UIFont(name: "Arial Italic", size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     // лейбл номера вопроса
     let questionNumberLabel: UILabel = {
         let label = UILabel()
+        label.layer.borderColor = CGColor.init(red: 1, green: 0.5, blue: 0.5, alpha: 1)
+        label.layer.borderWidth = 4
         label.text = "1"
         label.textColor = .black
         label.textAlignment = .center
-        label.frame = CGRect(x: 0, y: 0, width: 300, height: 10)
+        label.frame = CGRect(x: 0, y: 0, width: 71, height: 71)
+        label.layer.cornerRadius = label.frame.width / 2
         label.font = UIFont(name: "Arial Italic", size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // лейбл суммы вопроса
+    let questionSummLabel: UILabel = {
+        let label = UILabel()
+        label.layer.borderColor = CGColor.init(red: 1, green: 0.5, blue: 0.5, alpha: 1)
+        label.layer.borderWidth = 4
+        label.text = "Вопрос на сумму ..."
+        label.textAlignment = .center
+        label.layer.cornerRadius = 10
+        label.textColor = .black
+        label.frame = CGRect(x: 0, y: 0, width: 300, height: 80)
+        label.font = UIFont(name: "Arial Italic", size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -39,6 +80,7 @@ class GameViewController: UIViewController {
         label.textColor = .black
         label.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         label.font = UIFont(name: "Arial Italic", size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -53,22 +95,25 @@ class GameViewController: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.4513477087, green: 0.4857000113, blue: 0.5633345246, alpha: 1)
         playSound()
         setupUI()
+        roundTimer()
     }
     
-    // функция настройки кнопок опций
+    //
+    
+    // метод настройки кнопок опций
     func optionButtonSetup(button: UIButton) {
         button.layer.cornerRadius = 10
-        button.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
+//        button.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
         button.backgroundColor = UIColor.orange
         button.setTitleColor(.black, for: .normal)
         button.showsTouchWhenHighlighted = true
         button.titleLabel?.font = .systemFont(ofSize: 10)
+        button.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    // функция настройки кнопок ответа
+    // метод настройки кнопок ответа
     func answerButtonSetup(button: UIButton) {
         button.layer.cornerRadius = 10
-//        button.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
         button.backgroundColor = UIColor.black
         button.setTitleColor(.white, for: .normal)
         button.showsTouchWhenHighlighted = true
@@ -76,6 +121,22 @@ class GameViewController: UIViewController {
 
     }
     
+    // метод отсчета таймера
+    func roundTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimerLabel), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimerLabel() {
+        if secondPassed < totalTime {
+            totalTime -= 1
+            timerLabel.text = String(totalTime)
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    
+    // метод воспр. музыки
     func playSound() {
         guard let url = Bundle.main.url(forResource: "mainSound", withExtension: "mp3") else { return }
         do {
@@ -88,13 +149,14 @@ class GameViewController: UIViewController {
     
     //MARK: - UISetup
     func setupUI() {
+        
         // настройка кнопок опций
         optionButtonSetup(button: stopButton)
         optionButtonSetup(button: fiftyFiftyButton)
         optionButtonSetup(button: hallHelpButton)
         optionButtonSetup(button: callToFriendButton)
         
-        // настройка кноаок ответа
+        // настройка кнопок ответа
         answerButtonSetup(button: answerButtonA)
         answerButtonSetup(button: answerButtonB)
         answerButtonSetup(button: answerButtonC)
@@ -113,71 +175,71 @@ class GameViewController: UIViewController {
         answerButtonD.setTitle("D", for: .normal)
         
         // создаем стэк из кнопок опций
-        let optionStackView = UIStackView(arrangedSubviews: [stopButton, fiftyFiftyButton, callToFriendButton, hallHelpButton])
+        let optionStackView = UIStackView(arrangedSubviews: [fiftyFiftyButton, callToFriendButton, hallHelpButton])
         optionStackView.axis = .horizontal
         optionStackView.alignment = .fill
-        optionStackView.spacing = 10
+        optionStackView.spacing = 30
         optionStackView.distribution = .fillEqually
         optionStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        
-        // создаем вертикальный стэк из лейбла номера вопроса и лейбла с текстом вопроса
-        let labelStackView =  UIStackView(arrangedSubviews: [questionNumberLabel, questionLabel])
-        labelStackView.axis = .vertical
-        labelStackView.alignment = .fill
-        labelStackView.spacing = 0
-        labelStackView.distribution = .fillProportionally
-        labelStackView.translatesAutoresizingMaskIntoConstraints = false
         // создаем вертикальный стэк из кнопок выбора ответа
-        
         let answerStackView = UIStackView(arrangedSubviews: [answerButtonA, answerButtonB, answerButtonC, answerButtonD])
         answerStackView.axis = .vertical
         answerStackView.alignment = .fill
         answerStackView.spacing = 12
         answerStackView.distribution = .fillEqually
         answerStackView.translatesAutoresizingMaskIntoConstraints = false
-    
         
         // добавляем элементы на вью
-        view.addSubview(optionStackView)
-        view.addSubview(labelStackView)
-//        view.addSubview(questionNumberLabel)
-//        view.addSubview(questionLabel)
+        view.addSubview(timerLabel)
+        view.addSubview(stopButton)
+        view.addSubview(questionNumberLabel)
+        view.addSubview(questionSummLabel)
+        view.addSubview(questionLabel)
         view.addSubview(answerStackView)
+        view.addSubview(optionStackView)
+
         
         // расставляем констрейнты
         NSLayoutConstraint.activate([
             
-            optionStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            optionStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            optionStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            optionStackView.heightAnchor.constraint(equalToConstant: 40),
+            stopButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            stopButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            stopButton.heightAnchor.constraint(equalToConstant: 71),
+            stopButton.widthAnchor.constraint(equalToConstant: 71),
             
-
-//            questionNumberLabel.topAnchor.constraint(equalTo: optionStackView.bottomAnchor, constant: 16),
-//            questionNumberLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-//            questionNumberLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-//            questionNumberLabel.heightAnchor.constraint(equalToConstant: 20),
-//
-//            questionLabel.topAnchor.constraint(equalTo: questionNumberLabel.bottomAnchor, constant: 16),
-//            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-//            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-//            questionLabel.heightAnchor.constraint(equalToConstant: 190),
-
+            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            timerLabel.heightAnchor.constraint(equalToConstant: 71),
+            timerLabel.widthAnchor.constraint(equalToConstant: 71),
             
-            labelStackView.topAnchor.constraint(equalTo: optionStackView.bottomAnchor, constant: 0),
-            labelStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            labelStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            labelStackView.heightAnchor.constraint(equalToConstant: 200),
             
-            answerStackView.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 40),
-            answerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            answerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            answerStackView.heightAnchor.constraint(equalToConstant: 220)
-    
-        ])
+            questionNumberLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            questionNumberLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            questionNumberLabel.heightAnchor.constraint(equalToConstant: 71),
+            questionNumberLabel.widthAnchor.constraint(equalToConstant: 71),
+            
+            questionSummLabel.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: 30),
+            questionSummLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            questionSummLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            questionSummLabel.heightAnchor.constraint(equalToConstant: 60),
+            
+            questionLabel.topAnchor.constraint(equalTo: questionSummLabel.bottomAnchor, constant: 16),
+            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            questionLabel.heightAnchor.constraint(equalToConstant: 160),
+            
+            answerStackView.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 16),
+            answerStackView.bottomAnchor.constraint(equalTo: optionStackView.topAnchor, constant: -30),
+            answerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            answerStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            
+            optionStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            optionStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            optionStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            optionStackView.heightAnchor.constraint(equalToConstant: 30)
         
+        ])
     }
-    
+
 }
